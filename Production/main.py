8aaf3051476044eec976fbac775ce4ab26d5d443
roc-ket-cod-er <gargm0325@wrdsb.ca>
2025-05-30 18:49:20 -0400

    Update main.py

--- Production/main.py
+++ Production/main.py
@@ -1,33 +1,43 @@
+print("Starting")
 import board
-
 from kmk.kmk_keyboard import KMKKeyboard
-from kmk.scanners import DiodeOrientation
 from kmk.keys import KC
-from kmk.modules.macros import Press, Release, Tap, Macros
-
+from kmk.scanners import DiodeOrientation
+from kmk.modules.encoder import EncoderHandler
+from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.macros import Macros
 keyboard = KMKKeyboard()
-
+encoder_handler = EncoderHandler()
 macros = Macros()
+media = MediaKeys()
 keyboard.modules.append(macros)
-
-keyboard.col_pins = (board.A0, board.A1, board.A2)
-keyboard.row_pins = (board.GPIO1, board.GPIO2, board.GPIO3)
-
+keyboard.modules.append(encoder_handler)
+keyboard.extensions.append(media)
+# Matrix pins
+COL1 = board.A0
+COL2 = board.A1
+COL3 = board.A2
+ROW1 = board.D1
+ROW2 = board.D2
+ROW3 = board.D3
+# rotary encoder pins
+ROTA = board.A3
+ROTB = board.D4
+PUSHBUTTON = board.D9
+# Matrix settings
+keyboard.col_pins = (COL1, COL2, COL3)
+keyboard.row_pins = (ROW1, ROW2, ROW3)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
-
-
-
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
-
-keyboard.keymap = [
-    #[KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
-    [KC.N1, KC.N2, KC.N3], [KC.N4, KC.N5,
-                            
-    KC.N6], [KC.N7, KC.N8, KC.N9],
+# Encoder settings
+encoder_handler.pins = ((ROTA, ROTB, PUSHBUTTON, False),)
+encoder_handler.map = [
+    ((KC.VOLD, KC.VOLU, KC.MUTE),)
 ]
-
-# Start kmk!
-if __name__ == '__main__':
+keyboard.keymap = [
+    [
+        KC.A, KC.B, KC.C, KC.D, KC.E
+        KC.F, KC.G, KC.H, KC.I
+    ]
+]
+if __name__ == "__main__":
     keyboard.go()
